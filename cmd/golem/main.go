b2c8bc13a55bbf4e250e053a0cfdad6e9531f031
Advik <advik.b@gmail.com>
2025-06-25 21:59:04 +0530

    Update main.go

--- cmd/golem/main.go
+++ cmd/golem/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "github.com/Advik-B/Golem/internal/server"
-    "go.uber.org/zap"
+	server2 "github.com/Advik-B/Golem/server"
+	"go.uber.org/zap"
 )
 
 func main() {
-    server.InitLogger(true)
-    defer server.Log.Sync()
+	server2.InitLogger(true)
+	defer server2.Log.Sync()
 
-    server.Log.Info("Starting Golem server on :25565...")
-    srv := server.NewServer("0.0.0.0:25565")
-    if err := srv.Start(); err != nil {
-        server.Log.Fatal("Failed to start server", zap.Error(err))
-    }
+	server2.Log.Info("Starting Golem server on :25565...")
+	srv := server2.NewServer("0.0.0.0:25565")
+	if err := srv.Start(); err != nil {
+		server2.Log.Fatal("Failed to start server", zap.Error(err))
+	}
 }
