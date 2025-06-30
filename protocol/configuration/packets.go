1bda314efb8a72ef505fd97c91708cb36a3e7515
Advik <advik.b@gmail.com>
2025-07-01 01:08:04 +0530

    Refactor configuration packets for 1.21 protocol update
    Updated packet registration and IDs to align with Minecraft 1.21 Configuration state per wiki.vg. Removed SelectKnownPacks packets and related types, delegating more packets to the common package. Cleaned up unused code and improved import statements.

--- protocol/configuration/packets.go
+++ protocol/configuration/packets.go
@@ -2,127 +2,53 @@ package configuration
 
 import (
 	"github.com/Advik-B/Golem/protocol"
-	"github.com/Advik-B/Golem/protocol/codec"
+	"github.com/Advik-B/Golem/protocol/codec" // Corrected import
+	"github.com/Advik-B/Golem/protocol/common"
 )
 
 func init() {
 	sb := protocol.Registry[protocol.Configuration][protocol.Serverbound]
-	sb.Register(ServerboundFinishConfigurationPacketID, func() protocol.Packet { return &ServerboundFinishConfigurationPacket{} })
-	sb.Register(ServerboundSelectKnownPacksPacketID, func() protocol.Packet { return &ServerboundSelectKnownPacksPacket{} })
+	sb.Register(common.ServerboundClientInformationID, func() protocol.Packet { return &common.ServerboundClientInformationPacket{} })
+	// Cookie Response is registered in the cookie package's init
+	sb.Register(common.ServerboundCustomPayloadID, func() protocol.Packet { return &common.ServerboundCustomPayloadPacket{} })
+	sb.Register(common.ServerboundFinishConfigurationID, func() protocol.Packet { return &ServerboundFinishConfigurationPacket{} })
+	sb.Register(common.ServerboundKeepAliveID, func() protocol.Packet { return &common.ServerboundKeepAlivePacket{} })
+	sb.Register(common.ServerboundPongID, func() protocol.Packet { return &common.ServerboundPongPacket{} })
+	sb.Register(common.ServerboundResourcePackID, func() protocol.Packet { return &common.ServerboundResourcePackPacket{} })
 
 	cb := protocol.Registry[protocol.Configuration][protocol.Clientbound]
 	cb.Register(ClientboundFinishConfigurationPacketID, func() protocol.Packet { return &ClientboundFinishConfigurationPacket{} })
-	cb.Register(ClientboundSelectKnownPacksPacketID, func() protocol.Packet { return &ClientboundSelectKnownPacksPacket{} })
-	// TODO: Register RegistryData, UpdateEnabledFeatures
+	// TODO: Register other clientbound config packets
 }
 
+// Packet IDs specific to Configuration or that differ from Play state.
+// Using correct IDs per wiki.vg for 1.21 Configuration State
 const (
-	// Serverbound
-	ServerboundSelectKnownPacksPacketID    = 0x01
-	ServerboundFinishConfigurationPacketID = 0x02
-
-	// Clientbound
-	ClientboundSelectKnownPacksPacketID    = 0x04
 	ClientboundFinishConfigurationPacketID = 0x02
 )
 
-// KnownPack is a simplified version for this state.
-type KnownPack struct {
-	Namespace string
-	ID        string
-	Version   string
-}
+// ServerboundFinishConfigurationPacket is unique to this state.
+type ServerboundFinishConfigurationPacket struct{}
 
-func (kp *KnownPack) ReadFrom(r *codec.PacketBuffer) (err error) {
-	if kp.Namespace, err = r.ReadString(32767); err != nil {
-		return
-	}
-	if kp.ID, err = r.ReadString(32767); err != nil {
-		return
-	}
-	kp.Version, err = r.ReadString(32767)
-	return
+func (pk *ServerboundFinishConfigurationPacket) ID() int32 {
+	return common.ServerboundFinishConfigurationID
 }
-
-func (kp *KnownPack) WriteTo(w *codec.PacketBuffer) error {
-	w.WriteString(kp.Namespace)
-	w.WriteString(kp.ID)
-	w.WriteString(kp.Version)
+func (pk *ServerboundFinishConfigurationPacket) ReadFrom(r *codec.PacketBuffer) error {
+	return nil
+}
+func (pk *ServerboundFinishConfigurationPacket) WriteTo(w *codec.PacketBuffer) error {
 	return nil
 }
 
-// --- Clientbound ---
-
-type ClientboundSelectKnownPacksPacket struct {
-	KnownPacks []KnownPack
-}
-
-func (pk *ClientboundSelectKnownPacksPacket) ID() int32 { return ClientboundSelectKnownPacksPacketID }
-func (pk *ClientboundSelectKnownPacksPacket) ReadFrom(r *codec.PacketBuffer) error {
-	count, err := r.ReadVarInt()
-	if err != nil {
-		return err
-	}
-	pk.KnownPacks = make([]KnownPack, count)
-	for i := int32(0); i < count; i++ {
-		if err := pk.KnownPacks[i].ReadFrom(r); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-func (pk *ClientboundSelectKnownPacksPacket) WriteTo(w *codec.PacketBuffer) error {
-	w.WriteVarInt(int32(len(pk.KnownPacks)))
-	for _, pack := range pk.KnownPacks {
-		if err := pack.WriteTo(w); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
+// ClientboundFinishConfigurationPacket is unique to this state.
 type ClientboundFinishConfigurationPacket struct{}
 
 func (pk *ClientboundFinishConfigurationPacket) ID() int32 {
 	return ClientboundFinishConfigurationPacketID
 }
-func (pk *ClientboundFinishConfigurationPacket) ReadFrom(r *codec.PacketBuffer) error { return nil }
-func (pk *ClientboundFinishConfigurationPacket) WriteTo(w *codec.PacketBuffer) error  { return nil }
-
-// --- Serverbound ---
-
-type ServerboundSelectKnownPacksPacket struct {
-	KnownPacks []KnownPack
-}
-
-func (pk *ServerboundSelectKnownPacksPacket) ID() int32 { return ServerboundSelectKnownPacksPacketID }
-func (pk *ServerboundSelectKnownPacksPacket) ReadFrom(r *codec.PacketBuffer) error {
-	count, err := r.ReadVarInt()
-	if err != nil {
-		return err
-	}
-	pk.KnownPacks = make([]KnownPack, count)
-	for i := int32(0); i < count; i++ {
-		if err := pk.KnownPacks[i].ReadFrom(r); err != nil {
-			return err
-		}
-	}
+func (pk *ClientboundFinishConfigurationPacket) ReadFrom(r *codec.PacketBuffer) error {
 	return nil
 }
-func (pk *ServerboundSelectKnownPacksPacket) WriteTo(w *codec.PacketBuffer) error {
-	w.WriteVarInt(int32(len(pk.KnownPacks)))
-	for _, pack := range pk.KnownPacks {
-		if err := pack.WriteTo(w); err != nil {
-			return err
-		}
-	}
+func (pk *ClientboundFinishConfigurationPacket) WriteTo(w *codec.PacketBuffer) error {
 	return nil
 }
-
-type ServerboundFinishConfigurationPacket struct{}
-
-func (pk *ServerboundFinishConfigurationPacket) ID() int32 {
-	return ServerboundFinishConfigurationPacketID
-}
-func (pk *ServerboundFinishConfigurationPacket) ReadFrom(r *codec.PacketBuffer) error { return nil }
-func (pk *ServerboundFinishConfigurationPacket) WriteTo(w *codec.PacketBuffer) error  { return nil }
